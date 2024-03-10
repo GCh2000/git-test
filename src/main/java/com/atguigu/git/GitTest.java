f8e93d5dec2f43e71f2b19190c4dcc44882c1277
Sam <1293947641@qq.com>
2024-03-10 16:57:29 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.atguigu.git
+ * Description:
+ *
+ * @Author: 管成豪
+ * @Create: 2024/3/10 - 16:43
+ * @Version: v1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
